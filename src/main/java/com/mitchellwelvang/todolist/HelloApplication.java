feb390dd40e44d3bea08159dfaf4e9fafb6038b1
mwelvang <mwelvang1@gmail.com>
2024-06-13 09:23:26 -0500

    first

--- src/main/java/com/mitchellwelvang/todolist/HelloApplication.java
+++ src/main/java/com/mitchellwelvang/todolist/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        //first
     }
 
     public static void main(String[] args)
